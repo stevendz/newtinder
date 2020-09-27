4f301f10f14e6cc005d35f2ca8b00117b283c981
stevendz <steven.dzionara@gmail.com>
2020-09-27 13:56:21 +0200

    Create and style Header component

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from '@material-ui/core/IconButton'
 
-export default function Header() {
-    return (
-        <div>
-            <h2>I am a header!</h2>
-        </div>
-    )
+const Navbar = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 20px;
+`;
+const LogoImg = styled.img`
+height: 40px;
+`;
+
+class Header extends Component {
+    render() {
+        return (
+            <Navbar>
+                <IconButton onClick={() => { console.log('profile') }}>
+                    <PersonIcon fontSize='large' />
+                </IconButton>
+                <LogoImg src='https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png' alt='logo' />
+                <IconButton>
+                    <ForumIcon fontSize='large' />
+                </IconButton>
+            </Navbar>
+        )
+    }
 }
+
+export default Header;

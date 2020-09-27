6106a9224afa701bda37c190a96c6d6f847eedf4
stevendz <steven.dzionara@gmail.com>
2020-09-27 20:05:40 +0200

    Stream people from firestore database

--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TinderCard from 'react-tinder-card'
 import styled from 'styled-components'
+import database from '../firebase';
 import MyTinderCard from './MyTinderCard'
 
 const TinderCardsContainer = styled.div`
@@ -13,10 +14,17 @@ margin-top:5vh;
 `;
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        { name: 'steve jobs', url: 'https://swashvillage.org/storage/img/images_4/steve-jobs-biography_2.jpg' },
-        { name: 'mark zuckerberg', url: 'https://i.guim.co.uk/img/media/9eb0a1cd9f8abb5ef85b25ac1f62f02beb4c7efa/0_26_2048_1229/master/2048.jpg?width=700&quality=85&auto=format&fit=max&s=05591346110a83d0c679cab31c11304d' }
-    ])
+    const [people, setPeople] = useState([])
+    useEffect(() => {
+        const unsubscribe = database
+            .collection('people')
+            .onSnapshot(snapshot => (
+                setPeople(snapshot.docs.map(doc => doc.data()))
+            ))
+        return () => {
+            unsubscribe();
+        }
+    }, [people])
     return (
 
         <TinderCardsContainer>
